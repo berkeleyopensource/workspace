05e2eef62a3343e9481269b9dbe39a6208225ad9
Ariful Rigan <ari.rigan@gmail.com>
2020-04-15 00:16:22 -0700

    feat: add database dockerfile and init file

---
+++ database/initdb.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE users;
+
+USE users;
+
+CREATE TABLE Users (
+    username VARCHAR(20),
+    user_password VARCHAR(20)
+);
--- src/go/main.go
+++ src/go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/eecscord/workspace/src/go/auth"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
@@ -10,6 +11,21 @@ import (
 func main() {
 	//create http mux
 	mux := http.NewServeMux()
+
+	//database
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/users")
+	//db, err := sql.Open("mysql", "admin:admin@/users")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err.Error())
+	} else {
+		log.Println("pinged")
+	}
 
 	//register routes
 	registerAllRoutes(mux)

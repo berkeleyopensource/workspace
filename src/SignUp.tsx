b7c6982e22b237b7a0c871abd03711dfe3a006ab
berkenstock <nil@berkeley.edu>
2020-04-18 12:56:10 -0700

    :construction: sign in up pages

---
+++ src/SignUp.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './SignInUp.css';
+
+function App() {
+  return (
+    <div className="SignInUp">
+      <nav>
+        <h2>workspace</h2>
+      </nav>
+      <form>
+        <h2>Sign up.</h2>
+        <div className="input-group">
+          <div className="input-title">Email Address</div>
+          <input type="email"></input>
+        </div>
+        <div className="input-group">
+          <div className="input-title">Password</div>
+          <input type="password"></input>
+        </div>
+        <div className="input-group">
+          <button className="button-primary" type="submit"><div>Continue</div></button>
+        </div>
+        <div className="input-group">
+          <div><a href="/reset">Forgot your password?</a></div>
+          <div>Already have an account? <a href="/signup">Sign in.</a></div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
